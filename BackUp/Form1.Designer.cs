3a49cf9ac85d93e102eecbe3b7f992ef539f640d
unknown <shridharrathi@gmail.com>
2013-12-27 00:17:31 +1300

    Version 0.1
    The main purpose of this project is to create exact copy of
    the folders.
    
    Please check the BackUp –Utility –V 0.1.docx file for the screenshots

---
+++ BackUp/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BackUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create BackUp";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 61);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(680, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Dir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 126);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Target Dir";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 126);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(680, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(935, 275);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "BackUp - Utility - v 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ BackUp/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BackUp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("64-bit process = {0}", Environment.Is64BitProcess);
+            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox2.Text))
+            {
+                Directory.CreateDirectory(textBox2.Text);
+            }
+
+
+            delDir(textBox2.Text);
+            ProcessDirectory(textBox1.Text);
+            processFiles(textBox1.Text, textBox2.Text);
+
+
+            MessageBox.Show("Backup completed successfully!!", "BackUp - Utility - v 0.1");
+        }
+
+        private void ProcessDirectory(String srcPath)
+        {
+            String[] srcSubDir = Directory.GetDirectories(srcPath);
+
+
+            foreach (String path in srcSubDir)
+            {
+                ProcessDirectory(path);
+            }
+
+
+            if (Directory.Exists(srcPath.Replace(textBox1.Text, textBox2.Text)))
+            {
+                processFiles(srcPath, srcPath.Replace(textBox1.Text, textBox2.Text));
+
+            }
+            else
+            {
+                Directory.CreateDirectory(srcPath.Replace(textBox1.Text, textBox2.Text));
+                processFiles(srcPath, srcPath.Replace(textBox1.Text, textBox2.Text));
+            }
+
+
+
+            Console.WriteLine(srcPath);
+            Console.WriteLine(srcPath.Replace(textBox1.Text, textBox2.Text));
+
+        }
+
+        private void delDir(String destDirPath)
+        {
+
+            if (!Directory.Exists(destDirPath.Replace(textBox2.Text, textBox1.Text)))
+            {
+                Directory.Delete(destDirPath, true);
+                Console.WriteLine(destDirPath);
+            }
+            else
+            {
+                String[] destSubDir = Directory.GetDirectories(destDirPath);
+                foreach (String path in destSubDir)
+                {
+                    Console.WriteLine(path);
+                    delDir(path);
+
+                }
+
+            }
+        }
+
+        private void processFiles(String srcDirName, String destDirName)
+        {
+            String[] srcFileNames = getFileNames(srcDirName);
+
+            String[] destFileNames = getFileNames(destDirName);
+
+            copyChangedFiles(srcDirName, srcFileNames, destDirName, destFileNames);
+            copyNewFiles(srcDirName, srcFileNames, destDirName, destFileNames);
+            DeleteRemovedFiles(srcDirName, srcFileNames, destDirName, destFileNames);
+
+        }
+
+
+        private void copyChangedFiles(String srcDirName, String[] srcFileNames, String destDirName, String[] destFileNames)
+        {
+            String[] sCopyFiles = null;
+            sCopyFiles = srcFileNames.Intersect(destFileNames).ToArray<String>();
+
+            foreach (String path in sCopyFiles)
+            {
+                if (!Directory.GetLastWriteTimeUtc(srcDirName + path).Equals(Directory.GetLastWriteTimeUtc(destDirName + path)))
+                {
+                    System.IO.File.Delete(destDirName + path);
+                    System.IO.File.Copy(srcDirName + path, destDirName + path);
+                }
+            }
+
+        }
+
+        private void DeleteRemovedFiles(String srcDirName, String[] srcFileNames, String destDirName, String[] destFileNames)
+        {
+            String[] sCopyFiles = null;
+            sCopyFiles = destFileNames.Except(srcFileNames).ToArray<String>();
+
+            foreach (String path in sCopyFiles)
+            {
+                System.IO.File.Delete(destDirName + path);
+
+            }
+
+        }
+
+        private void copyNewFiles(String srcDirName, String[] srcFileNames, String destDirName, String[] destFileNames)
+        {
+            String[] sCopyFiles = null;
+            sCopyFiles = srcFileNames.Except(destFileNames).ToArray<String>();
+
+            foreach (String path in sCopyFiles)
+            {
+                System.IO.File.Copy(srcDirName + path, destDirName + path);
+            }
+        }
+
+        private String[] getFileNames(String directoryName)
+        {
+            String[] srcFileNames = Directory.GetFiles(directoryName);
+            List<String> sActualFileName = new List<String>();
+            foreach (String path in srcFileNames)
+            {
+                sActualFileName.Add(path.Replace(directoryName, ""));
+            }
+
+            return sActualFileName.ToArray<String>();
+
+        }
+
+       
+
+      
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // OK button was pressed. 
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+
+            // OK button was pressed. 
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+            }
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // OK button was pressed. 
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+
+            // OK button was pressed. 
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        
+
+    }
+}
